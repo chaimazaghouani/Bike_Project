dd5ea87d9abbe38441fb345fb255fad93666850a
chaima <110690177+chaimazaghouani@users.noreply.github.com>
2023-12-28 18:28:08 +0100

    commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,12 @@
+# Exécution de elasticsearch :
+spark-submit \
+  --master local[*]\
+  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.elasticsearch:elasticsearch-spark-30_2.12:8.8.2 \
+  --conf "spark.jars.packages=org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.elasticsearch:elasticsearch-spark-30_2.12:8.8.2" \
+  pyspark_consumer_elasticsearch.py > out.txt
+
+
+# Exécution de hive :
 spark-submit \
   --master local[*] \
   --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.apache.spark:spark-hive_2.12:3.2.4 \
