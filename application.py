2eaf302278ae0b350a32a6386e8763703250ae07
chaima zaghouani <chaima.zaghouani@supcom.tn>
2023-12-28 18:36:31 +0100

    Renommer app.py en application.py

---
+++ application.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+# Titre de l'application
+st.title("Real Time Bike Project")
+
+# Image de Kibana
+kibana_image = 'kibana.png'
+st.image(kibana_image, caption='Kibana', use_column_width=True)
+
+# Bouton pour Kibana
+if st.button("Accéder à Kibana"):
+    st.write("Redirection vers http://localhost:5601")
+
+# Saut de ligne pour la mise en forme
+st.write("\n")
+
+# Image de Hive
+hive_image = 'hive.png'
+st.image(hive_image, caption='Hive', use_column_width=True)
+
+# Bouton pour Hive
+if st.button("Accéder à Hive"):
+    st.write("Redirection vers http://localhost:50070")
+
+# Saut de ligne pour la mise en forme
+st.write("\n")
+
+# Bouton pour afficher l'architecture du projet
+if st.button("Afficher l'architecture du projet"):
+    project_architecture_image = 'pipline.png'
+    st.image(project_architecture_image, caption='Architecture du projet', use_column_width=True)
